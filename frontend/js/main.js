55bd16a609ad4006e1ae6f48c198d08fdddfcb97
esthercheran <66366384+esthercheran@users.noreply.github.com>
2021-01-03 12:28:21 +0530

    latest

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -87,10 +87,10 @@ $(".nav-link").on("click", function () {
           // CARD TILT ANIMATION EFFECT - TILT JS
           */
 const tilt = $(".ds-tilt").tilt({
-  maxTilt: 10,
+  maxTilt: 25,
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
   easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
-  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  scale: 1.3, // 2 = 200%, 1.5 = 150%, etc..
   speed: 300, // Speed of the enter/exit transition.
   transition: true, // Set a transition on enter/exit.
   disableAxis: null, // What axis should be disabled. Can be X or Y.
